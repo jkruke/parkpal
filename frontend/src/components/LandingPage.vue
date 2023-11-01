b6314a7c3568e3502a08546eeb2f677f5824ea7e
Jonas Krukenberg <jonas@jkrukenberg.de>
2023-11-01 09:09:05 +0700

    initialize frontend (vue.js)

---
+++ frontend/src/components/LandingPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="header">
+    <span class="left">IoT - ParkPal</span>
+  </div>
+  <div id="content">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage'
+}
+</script>
+
+<style scoped>
+#header {
+  background: #164195;
+  padding: 1em;
+  color: white;
+  font-weight: bold;
+  text-align: left;
+}
+</style>
