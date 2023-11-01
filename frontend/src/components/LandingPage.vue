a3f0072b002d227e60c61b3f4a1cf2ac2a7a9c4a
Jonas Krukenberg <jonas@jkrukenberg.de>
2023-11-01 11:44:17 +0700

    add example code for our use cases

--- frontend/src/components/LandingPage.vue
+++ frontend/src/components/LandingPage.vue
@@ -3,21 +3,30 @@
     <span class="left">IoT - ParkPal</span>
   </div>
   <div id="content">
-
+    <ParkingLotPanel :id="id" v-for="id in parkingLots" :key="id" />
+    <div>Parking recommendation: ...</div>
   </div>
 </template>
 
 <script>
+import ParkingLotPanel from "@/components/ParkingLotPanel.vue";
+
 export default {
-  name: 'LandingPage'
+  name: 'LandingPage',
+  components: {ParkingLotPanel},
+  data() {
+    return {
+      parkingLots: [0] // IDs of all parking lots for which we want to display a panel
+    }
+  }
 }
 </script>
 
 <style scoped>
 #header {
-  background: #164195;
+  background: var(--blue);
   padding: 1em;
-  color: white;
+  color: var(--white);
   font-weight: bold;
   text-align: left;
 }
