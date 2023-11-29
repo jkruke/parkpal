a688c0ca6b3c5f7f0a1225c5a997421abef449db
Trinh V. Nguyen <nguyenvantrinh2607@gmail.com>
2023-11-29 11:39:41 +0700

    update apis

---
+++ go.mod
@@ -0,0 +1,19 @@
+module parkpal-web-server
+
+go 1.21.1
+
+require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2
+	github.com/gorilla/handlers v1.5.2
+	github.com/gorilla/mux v1.8.1
+	github.com/hashicorp/go-hclog v1.5.0
+	github.com/nicholasjackson/env v0.6.1
+)
+
+require (
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+)
---
+++ internal/business/business.go
@@ -0,0 +1,106 @@
+package business
+
+import (
+	"context"
+	"parkpal-web-server/internal/entity"
+	"time"
+
+	"github.com/hashicorp/go-hclog"
+)
+
+type GetParkingLotRequest struct {
+	ID int `json:"id"`
+}
+
+type GetParkingLotResponse entity.ParkingLot
+
+type GetAllParkingLotResponse []entity.ParkingLot
+
+type SearchParkingLotResponse entity.ParkingLot
+
+type UpdateParkingLotRequest entity.ParkingLot
+
+type UpdateParkingLotResponse entity.ParkingLot
+
+type Business interface {
+	GetParkingLot(ctx context.Context, request *GetParkingLotRequest) (*GetParkingLotResponse, error)
+	GetAllParkingLots(ctx context.Context) (*GetAllParkingLotResponse, error)
+	SearchParkingLot(ctx context.Context, name string) (*SearchParkingLotResponse, error)
+	UpdateParkingLot(ctx context.Context, request *UpdateParkingLotRequest) (*UpdateParkingLotResponse, error)
+}
+
+type Repository interface {
+	GetParkingLotByID(ctx context.Context, id int) (*entity.ParkingLot, error)
+	GetAllParkingLots(ctx context.Context) ([]*entity.ParkingLot, error)
+	GetParkingLotByName(ctx context.Context, name string) (*entity.ParkingLot, error)
+	UpdateParkingLotByID(ctx context.Context, pl entity.ParkingLot) (*entity.ParkingLot, error)
+}
+
+type business struct {
+	repository Repository
+	timeout    time.Duration
+	l          hclog.Logger
+}
+
+func NewBusiness(repository Repository, timeout time.Duration, l hclog.Logger) *business {
+	return &business{
+		repository,
+		timeout,
+		l,
+	}
+}
+
+func (b *business) SearchParkingLot(c context.Context, name string) (*SearchParkingLotResponse, error) {
+	ctx, cancel := context.WithTimeout(c, b.timeout)
+	defer cancel()
+
+	prod, err := b.repository.GetParkingLotByName(ctx, name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &SearchParkingLotResponse{prod.ID, prod.Name, prod.BikeCount, prod.CongestionRate}, nil
+}
+
+func (b *business) GetAllParkingLots(c context.Context) (*GetAllParkingLotResponse, error) {
+	ctx, cancel := context.WithTimeout(c, b.timeout)
+	defer cancel()
+
+	prods, err := b.repository.GetAllParkingLots(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a new slice of entity.ParkingLot
+	response := make(GetAllParkingLotResponse, len(prods))
+
+	// Copy values from prodsPointers to prods
+	for i, ptr := range prods {
+		response[i] = *ptr
+	}
+	return &response, nil
+}
+
+func (b *business) GetParkingLot(c context.Context, request *GetParkingLotRequest) (*GetParkingLotResponse, error) {
+	ctx, cancel := context.WithTimeout(c, b.timeout)
+	defer cancel()
+
+	prod, err := b.repository.GetParkingLotByID(ctx, request.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &GetParkingLotResponse{prod.ID, prod.Name, prod.BikeCount, prod.CongestionRate}, nil
+}
+
+func (b *business) UpdateParkingLot(c context.Context, request *UpdateParkingLotRequest) (*UpdateParkingLotResponse, error) {
+	ctx, cancel := context.WithTimeout(c, b.timeout)
+	defer cancel()
+
+	prod, err := b.repository.UpdateParkingLotByID(ctx, entity.ParkingLot(*request))
+	if err != nil {
+		return nil, err
+	}
+
+	return &UpdateParkingLotResponse{prod.ID, prod.Name, prod.BikeCount, prod.CongestionRate}, nil
+}
